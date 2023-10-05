6e4a715464f61e911793373964425a3d3dd0fa37
Linkan Sahu <sahulinkan7@gmail.com>
2023-10-05 17:40:46 +0530

    updated dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.11.4-alpine
+
+WORKDIR todoapp/
+
+COPY . .
+
+RUN pip3 install -r requirements.txt
+EXPOSE 8000:8000
+CMD ["python3","manage.py","runserver","0.0.0.0:8000"]
+
--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ww=sr%!fv*oqr1w$+$bh^@o_l=l@a$_!xa8^m-ygplrzv_pz($
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
