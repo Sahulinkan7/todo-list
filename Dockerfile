fc1cb8b639078867ddc788d4c01542cea042b3bb
Linkan Sahu <sahulinkan7@gmail.com>
2023-10-17 18:55:47 +0530

    updated docker compose

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /todoapp
 
 COPY . .
 
+RUN apt-get update 
 RUN pip3 install -r requirements.txt
 EXPOSE 8000:8000
 CMD ["python3","manage.py","runserver","0.0.0.0:8000"]
--- todolist/settings.py
+++ todolist/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +77,11 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.environ.get('SQL_NAME'),
+        'USER': os.environ.get('SQL_USER'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': os.environ.get('PORT'),
     }
 }
 
